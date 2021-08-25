c853ce2d37562b0f9f7a89b1500ef1c996687631
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-25 02:01:13 -0700

    contain java code for java formatting
    contain java code for java formatting

---
+++ JavaFormatting.java
@@ -0,0 +1,20 @@
+
+class JavaFormatting
+{
+public static void main(String args[])
+{
+	int x = 789;
+	System.out.printf("Printing simple integer: x = %d\n", x);
+
+	System.out.printf("Formatted with precison: PI = %.2f\n", Math.PI);
+
+	float n = 5.2f;	
+	System.out.printf("Formatted to specific width: n = %.4f\n", n);
+       
+        System.out.printf("Formatted with precison: PI = %.2f\n", Math.PI);
+        n = 2324435.3f;
+ 	System.out.printf("Formatted to right margin: n = %20.4f\n", n);
+        
+        }
+       
+     }
