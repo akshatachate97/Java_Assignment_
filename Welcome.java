4ef82e8df60f97b0d5bb9248a6ce5794228faaa9
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-25 00:45:49 -0700

    Contain java code to Print Welcome
    Contain java code to Print Welcome

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+// 1 Write a program in JAVA to print a welcome text in seperate line
+
+
+
+public class Welcome
+   {
+       
+     public static void main( String[] args )
+    {
+        System.out.println( " Welcome to " );
+        System.out.println( " java programming " );
+    }
+  }
