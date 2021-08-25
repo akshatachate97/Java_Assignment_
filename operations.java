4a3d507951528fcc03321804a18e035c991d9604
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-25 07:03:54 -0700

    contains java code for specified operations
    contains java code for specified operations

---
+++ operations.java
@@ -0,0 +1,19 @@
+
+ //  Write a program in JAVA to print the result of the specified operations.   
+
+
+ public class operations {
+ 
+ public static void main(String[] args)
+ {
+  System.out.println("Result of 1st expression is:");
+  System.out.println(-6 + 9 * 7);
+  System.out.println("Result of 2nd expression is:");
+  System.out.println((54+7) % 8);
+  System.out.println("Result of 3rd expression is:");
+  System.out.println(22 + -2*5 / 9);
+  System.out.println("Result of 4th expression is:");
+  System.out.println(6 + 17 / 3 * 2 - 8 % 3);
+ }
+ 
+}
