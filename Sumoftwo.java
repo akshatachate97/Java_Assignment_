17b39bd6574a93e67837c9c935da01abbc568bf5
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-25 00:53:32 -0700

    Contain java code for sum of two numbers
    Contain java code for sum of two numbers

---
+++ Sumoftwo.java
@@ -0,0 +1,15 @@
+// 2. Write a program in java to print the sum of two numbers
+
+
+public class Sumoftwo {
+
+   public static void main(String[] args) {
+        
+      int num1 = 55, num2 = 158, sum;
+      sum = num1 + num2;
+
+   System.out.println("Print the sum of two numbers:");      
+   System.out.println("------------------------------");
+   System.out.println("Sum of 55 and 158 is: "+sum);
+   }
+}
