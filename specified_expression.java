2a6204ca43b48beb5aaf29502fa0782a1e7c61d6
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-28 09:47:58 -0700

    JAVA code to compute the specified expressions
    JAVA code to compute the specified expressions

---
+++ specified_expression.java
@@ -0,0 +1,11 @@
+    //Write a program in JAVA to compute the specified expressions and print the output
+
+
+public class specified_expression
+ {
+    public static void main(String[] args) 
+{
+        double result = (55.5 * 8.5 - 3.5 * 10.6) / (55.5 - 4.5);
+        System.out.println("Result of the expression is " +result); //
+    }
+}
