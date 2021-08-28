bce44a308372035f285d8eb7adba2c60baa98ea3
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-28 09:50:06 -0700

    JAVA code to prints  multiplication table
    contain JAVA code that takes a number as input and prints its multiplication table to 10

---
+++ multiplication_table.java
@@ -0,0 +1,24 @@
+   //Write a program in JAVA that takes a number as input and prints its multiplication table upto 10
+
+
+
+import java.util.Scanner;
+ 
+public class multiplication_table
+ {
+ 
+ public static void main(String[] args)
+ {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input a number: ");
+  int num1 = in.nextInt();
+   
+  for (int i=0; i< 10; i++){
+   System.out.println(num1 + " x " + (i+1) + " = " + 
+    (num1 * (i+1)));
+       }
+  }
+
+}
+   
