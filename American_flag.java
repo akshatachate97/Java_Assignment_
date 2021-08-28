5ddd8f5be335d610f518b5683f15eff592caa1e4
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-28 09:52:43 -0700

    JAVA code to print an American flag on the screen
    contain JAVA code to print an American flag on the screen

---
+++ American_flag.java
@@ -0,0 +1,24 @@
+   //Write a program in JAVA to print an American flag on the screen
+
+
+public class American_flag {
+ 
+   public static void main(String[] args)
+    {
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
