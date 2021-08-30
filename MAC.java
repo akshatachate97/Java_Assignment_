a332c3be266cb1c6771f961b5caf23d69e608d96
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-30 00:33:20 -0700

    contain java code

---
+++ MAC.java
@@ -0,0 +1,17 @@
+/*Create a class named 'Student' with String variable 'name' and integer variable 'roll_no'
+ Assign the value of roll_no as '2' and that of name as "John" by creating an object of the class Student*/
+
+
+class Student{
+  String name;
+  int roll_no;
+}
+
+class MAC{
+  public static void main(String[] args){
+    Student s = new Student();
+    s.name = "John";
+    s.roll_no = 2;
+    System.out.println("Name is "+s.name+" and roll number is "+s.roll_no);
+  }
+}
