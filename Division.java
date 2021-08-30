c65f63a3364daa4d50501fead569079ed77b558e
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-30 06:31:38 -0700

    contain java code

---
+++ Division.java
@@ -0,0 +1,12 @@
+  //Write a program in JAVA to divide two numbers and print on the screen
+
+   
+public class Division
+{
+public static void main(String args[])
+{
+int num1=88,num2=55;
+int result=num1/num2;
+System.out.println("Division of "+num1+" and "+num2+" is:"+result);
+}
+}
