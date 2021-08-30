fcc3da3e7706454a2c5689326e3556685c9859e4
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-29 21:09:20 -0700

    contain java code for hexagon area

---
+++ Area_Hexagon.java
@@ -0,0 +1,16 @@
+//Write a program in JAVA to print the area of a hexagon.   
+
+
+import java.util.Scanner;
+public class Area_Hexagon 
+{
+      public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the length of a side of the hexagon: ");
+        double s = input.nextDouble();
+        System.out.print("The area of the hexagon is: " + hexagonArea(s)+"\n");
+    }
+    public static double hexagonArea(double s) {
+        return (6*(s*s))/(4*Math.tan(Math.PI/6));
+    }
+}
