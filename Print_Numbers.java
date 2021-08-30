aed9d1128858c8b38a11fa1077f38e58019f0be3
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-29 18:25:07 -0700

    java code to print numbers from 1 to 10
    contain java code  to print numbers from 1 to 10

---
+++ Print_Numbers.java
@@ -0,0 +1,13 @@
+  //Write a program to print numbers from 1 to 10
+
+
+public class Print_Numbers
+{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
