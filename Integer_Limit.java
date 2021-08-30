85317ad545cbcd54d3c4284355854ca2f6435332
akshatachate97 <88848689+akshatachate97@users.noreply.github.com>
2021-08-30 01:28:24 -0700

    contain java code

---
+++ Integer_Limit.java
@@ -0,0 +1,17 @@
+ //Write a program in JAVA to check the upper and lower limits of integer  
+
+class Integer_Limit 
+{ 
+public static void main (String[] args)
+ {
+
+System.out.println( "Check the upper and lower limits of integer :"); 
+System.out.println( "-------------------------------------"); 	
+System.out.println("The maximum limit of int data type:" +Integer.MAX_VALUE);  
+System.out.println("The minimum limit of int data type:" + Integer.MIN_VALUE);
+  System.out.println("The Bits contain in char data type:"+Character.SIZE );
+System.out.println("The minimum limit of short data type:" + Short.MIN_VALUE);
+System.out.println("The maximum limit of short data type:" +Short.MAX_VALUE); 
+  
+    }
+}
